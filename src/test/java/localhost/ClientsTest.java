f0f077f1f746f8d6d38627aa181a379f0594d857
Giovannise <giovannise_lisboa@hotmail.com>
2021-12-04 16:30:01 -0300

    Cria o teste do get com body

--- src/test/java/localhost/ClientsTest.java
+++ src/test/java/localhost/ClientsTest.java
@@ -2,6 +2,7 @@ package localhost;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.*;
 
 public class ClientsTest {
 
@@ -14,5 +15,28 @@ public class ClientsTest {
                 .then()
                 .log().all()
                 .statusCode(200);
+    }
+
+    @Test
+    void testUnicoClientes_ClienteEListadoComTodasAsInformacoes() {
+        RestAssured.given()
+                .log().all()
+                .when()
+                .get("http://localhost:8080/clients/1")
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    void testUnicoClientes_ClienteEListadoComTodasAsInformacoesComBody() {
+        RestAssured.given()
+                .log().all()
+                .when()
+                .get("http://localhost:8080/clients/1")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("id", is(1));
     }
 }
