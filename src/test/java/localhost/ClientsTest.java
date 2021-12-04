25064901b4c9c63f6220c39f8385761046a2f36d
Giovannise <giovannise_lisboa@hotmail.com>
2021-12-04 16:55:55 -0300

    Incluído teste com POST

--- src/test/java/localhost/ClientsTest.java
+++ src/test/java/localhost/ClientsTest.java
@@ -1,6 +1,7 @@
 package localhost;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.Matchers.*;
@@ -26,7 +27,7 @@ public class ClientsTest {
 
     /*
     @Test
-    void testUnicoClientes_ClienteEListadoComTodasAsInformacoes() {
+    void testListaUnicoCliente_ClienteEListadoComTodasAsInformacoes() {
         RestAssured.given()
                 .log().all()
                 .when()
@@ -38,7 +39,7 @@ public class ClientsTest {
      */
 
     @Test
-    void testUnicoClientes_ClienteEListadoComTodasAsInformacoesComBody() {
+    void testListaUnicoCliente_ClienteEListadoComTodasAsInformacoesComBody() {
 
         int id = 1;
 
@@ -50,5 +51,25 @@ public class ClientsTest {
                 .log().all()
                 .statusCode(200)
                 .body("id", is(id));
+    }
+
+    @Test
+    void testInlcuiCliente_ClienteEIncluidoComSucesso() {
+
+        String email = "senhorateste@teste.com";
+        String name = "Teste";
+
+        RestAssured.given()
+                .body("{\n" +
+                        "        \"email\": \""+ email +"\", \n" +
+                        "        \"name\": \""+ name +"\" \n" +
+                        "    }")
+                .log().all()
+                .contentType(ContentType.JSON)
+                .when()
+                .post() //pode botar só o que vem depois de clients na URL porque ela já está setado no BeforeAll
+                .then()
+                .log().all()
+                .statusCode(201);
     }
 }
